4be67acab907b2b7b93176a71c2ee815927c7382
Nick Barth <nickbarth@live.com>
2019-03-20 17:46:59 -0600

    Create tkslack.rb

---
+++ tkslack.rb
@@ -0,0 +1,10 @@
+cask 'tkslack' do
+  version '1.0.0'
+  sha256 '80de78554ae431ca32a6fc1d00e1b8c37563131302f59452c67f466813ad4a05'
+
+  name 'tkslack'
+  url "https://github.com/nickbarth/tkslack/releases/download/v1.0.0/tkslack-mac.zip"
+  homepage 'https://github.com/nickbarth/tkslack/'
+  
+  app 'TKSlack.app'
+end
